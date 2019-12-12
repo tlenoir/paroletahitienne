e638c7963c3106a4ea2c76cd7fd4bc47fb43eb31
tlenoir <tlenoir.tcc@gmail.com>
2019-12-12 12:02:43 -1000

    Updates

---
+++ precache-manifest.9c7710ead119ccb2e0eaff7edd9d8fe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c7f7ad5ef88b429dde8449df3e20cc2",
+    "url": "/paroletahitienne/index.html"
+  },
+  {
+    "revision": "5cc8dd0f86e08ef88910",
+    "url": "/paroletahitienne/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1e59e80beeb8f71409dc",
+    "url": "/paroletahitienne/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5cc8dd0f86e08ef88910",
+    "url": "/paroletahitienne/static/js/2.abbccc9f.chunk.js"
+  },
+  {
+    "revision": "7c0b098a7fb85fa75d309e01c132837c",
+    "url": "/paroletahitienne/static/js/2.abbccc9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e59e80beeb8f71409dc",
+    "url": "/paroletahitienne/static/js/main.ac6df02b.chunk.js"
+  },
+  {
+    "revision": "817714094209961e07d8",
+    "url": "/paroletahitienne/static/js/runtime-main.092dcadc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3523938f61753ee32895b5361b921ce3.js"
+  "/paroletahitienne/precache-manifest.9c7710ead119ccb2e0eaff7edd9d8fe4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/paroletahitienne/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
